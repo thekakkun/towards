5fa3d2533a5a85bf79404553662fb823241b571b
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-02-25 23:26:09 -0500

    Set location state to prompt if service available

--- src/hooks/useCoordinates.ts
+++ src/hooks/useCoordinates.ts
@@ -24,6 +24,7 @@ export default function useCoordinates(): SensorHook<Coordinates> {
     if (!("geolocation" in navigator)) {
       setSensorState("unavailable");
     } else {
+      setSensorState("prompt");
       attachListener();
     }
 
