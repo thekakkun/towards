1619a93a7c0779f12aa1914a62afcccb9035fb50
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-11-14 15:23:37 -0500

    Add distance functions to be defined during seed

--- backend/seeders/seedLocation.ts
+++ backend/seeders/seedLocation.ts
@@ -5,6 +5,7 @@ import { dbConfig } from "../src/config/database";
 const pool = new Pool(dbConfig);
 
 seedLocationTable();
+createDistanceFunction();
 
 async function seedLocationTable() {
   // Recreate table
@@ -38,4 +39,32 @@ async function seedLocationTable() {
   await pool.end();
 }
 
-createAndInsert();
+async function createDistanceFunction() {
+  // Recreate haversine function
+  await pool.query("DROP FUNCTION IF EXISTS hav CASCADE;");
+  await pool.query(`CREATE FUNCTION
+  hav(theta real) RETURNS real
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT
+    RETURN sind(theta / 2) ^ 2;`);
+
+  // Recreate spherical distance function
+  await pool.query("DROP FUNCTION IF EXISTS spherical_distance CASCADE;");
+  await pool.query(`CREATE FUNCTION
+  spherical_distance(lat1 real, lon1 real, lat2 real, lon2 real) RETURNS real
+  LANGUAGE SQL
+  IMMUTABLE
+  RETURNS NULL ON NULL INPUT
+  RETURN 2 * 6371 * asin( -- Multiply central angle by earth's diameter
+    sqrt(
+      least( -- Clamp [0, 1] in case it exeeds ranges due to floating point error
+        greatest(
+          0.0,
+          hav(lat2 - lat1) + cosd(lat1) * cosd(lat2) * hav(lon2 - lon1)
+        ),
+        1.0
+      )
+    )
+  );`);
+}
